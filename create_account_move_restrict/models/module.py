d9d25d61da0a87c2dc319ee3334b679156264a02
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-09-13 23:08:53 +0300

    Update module.py

--- create_account_move_restrict/models/module.py
+++ create_account_move_restrict/models/module.py
@@ -16,16 +16,16 @@ from odoo.exceptions import UserError
 class AccountMove(models.Model):
     _inherit = 'account.move'
    
-    @api.onchange('source_id','move_type')
-    def onchange_source_id(self):
+    @api.onchange('invoice_origin','move_type')
+    def onchange_invoice_origin(self):
         for move in self:
             user_invoice = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('create_account_move_restrict.create_invoice_restrict').id)])
             user_bill = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('create_account_move_restrict.create_bill_restrict').id)])
             user_entry = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('create_account_move_restrict.create_entry_restrict').id)])
-            if not user_invoice and not move.source_id and move.move_type =='out_invoice':
+            if not user_invoice and not move.invoice_origin and move.move_type =='out_invoice':
                 raise UserError(_('Not Allowed To Create Manual Invoice'))
-            if not user_invoice and not move.source_id and move.move_type =='in_invoice':
+            if not user_invoice and not move.invoice_origin and move.move_type =='in_invoice':
                 raise UserError(_('Not Allowed To Create Manual Bill'))
-            if not user_invoice and not move.source_id and move.move_type =='entry':
+            if not user_invoice and not move.invoice_origin and move.move_type =='entry':
                 raise UserError(_('Not Allowed To Create Manual Journal Entry'))
 
